ce25b0a9bd69bb35349c5454926b9660b8c31b72
camilo.rivera <camilo.rivera@globant.com>
2020-01-17 09:05:06 -0500

    inputs and outputs

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,27 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from 'src/interfaces/product.model';
+
 
 @Component({
   selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  templateUrl: './product.component.html'
 })
+export class ProductComponent {
 
-export class ProductComponent implements OnInit {
+  @Input() product: Product;
+  @Output() productClicked: EventEmitter<any> = new EventEmitter();
 
-  product: Product[] = [
-    {
-      id: '1',
-      image: 'assets/images/camiseta.png',
-      title: 'Camiseta',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    }
-  ];
-
-  constructor() { }
-
-  ngOnInit() {
+  addCart() {
+    console.log('añadir al carrito');
+    this.productClicked.emit(this.product.id);
   }
 
 }
